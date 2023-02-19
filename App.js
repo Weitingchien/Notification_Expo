825d71de2cc971be1773269ae87db105fe295ff6
Ting <chienweiting3839@gmail.com>
2023-02-19 20:34:03 +0800

    Add Button and ImageViewer

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as ImagePicker from 'expo-image-picker';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <View>
+        <Button theme="primary" label="選擇您的課表" onPress={pickImageAsync} />
+      </View>
+      <ImageViewer
+        placeholderImageSource={PlaceholderImage}
+        selectedImage={selectedImage}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
 });
---
+++ components/ImageViewer.js
@@ -0,0 +1,6 @@
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+}
